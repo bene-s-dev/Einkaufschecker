3484b68378d0d7aadde8c28d80f29f0c701d7d58
bene-s-dev <282218590+bene-s-dev@users.noreply.github.com>
2026-06-09 00:02:58 +0200

    Delete SeviceWorker.js

--- SeviceWorker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('einkaufschecker-v1').then((cache) => {
-            return cache.addAll(['./index.html', './manifest.json']);
-        })
-    );
-});
